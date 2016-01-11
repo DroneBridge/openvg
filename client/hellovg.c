36c3184d9d8988f8fbe96816a6abd21e2447afd0
Paeryn <paeryn@gmail.com>
2016-01-11 21:46:48 +0000

    Revert "Reverted back to original."
    This reverts commit cb50ee3c935f147a0c62f44bf6f2e69712d2a5a2.

--- client/hellovg.c
+++ client/hellovg.c
@@ -24,6 +24,24 @@ int main() {
 	TextMid(width / 2, (height * 0.5), hello1 , SerifTypeface, width / 15);
 	TextMid(width / 2, (height * 0.3), hello2 , SerifTypeface, width / 15);
 	TextMid(width / 2, (height * 0.1), hello3 , SerifTypeface, width / 15);
+// Test clearing an area the width of an 'A' in the serif font 10pt
+// First I'll draw it, then clear over the top of it so it shouldn't appear
+// on the screen. Also test the colour defines.
+// Red background so we can see the area cleared
+	Fill(SH_red, 1);
+	Rect(30, 30, 50, 50);
+// Green text, draw 2 letter A, the second so we can see what it looked like
+	Fill(SH_green, 1);
+	Text(40, 40, "AA", SerifTypeface, 16);
+	// Size the font goes below the baseline (descender)
+	font_depth = TextDepth(SerifTypeface, 16);
+	// Size the font goes above the baseline (height)
+	font_height = TextHeight(SerifTypeface, 16);
+	// Width of an 'A'
+	font_width = TextWidth("A", SerifTypeface, 16);
+	// Total height to clear starts from baseline-depth to depth+height
+	AreaClear(40, 40-font_depth, font_width, font_depth+font_height);
+
 	End();						   // End the picture
 
 	fgets(s, 2, stdin);				   // look at the pic, end with [RETURN]
